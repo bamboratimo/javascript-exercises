a6ad50c836e4684b3e74279d384ded426c2ce036
Timo Rautiainen <timo.rautiainen94@gmail.com>
2022-10-11 22:57:40 +0300

    js complete without testing

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = [];
+    for (let i = 0; i < string.length; i++) {
+        arr.unshift(string.charAt(i));
+    }
+    let final = arr.join("");
+    return final;
 };
+reverseString("moi");
 
 // Do not edit below this line
 module.exports = reverseString;
