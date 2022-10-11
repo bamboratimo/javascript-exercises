50e83bc86c85c95038e5b01c1455818fdd574270
Timo Rautiainen <timo.rautiainen94@gmail.com>
2022-10-11 21:26:32 +0300

    first test works

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let moi = "";
+    for (let i = 1; i <= num; i++) {
+        moi += string;
+    }
+    return moi;
 };
 
 // Do not edit below this line
