e8e215fe7d2ec3c2ea57f13fd2a11a824b721743
Timo Rautiainen <timo.rautiainen94@gmail.com>
2022-10-15 23:35:34 +0300

    all tests work

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,7 @@
 const removeFromArray = function(arr) {
     for (let i = arr.length - 1; i >= 0; i--) {
         for (let j = arguments.length - 1; j > 0; j--) {
-            if (arr[i] == arguments[j]) {
+            if (arr[i] === arguments[j]) {
                 arr.splice(i, 1);
             }
         }
