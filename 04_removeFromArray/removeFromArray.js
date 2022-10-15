01a2b20d7a8b017406d1d639b1600067d1a6fef7
Timo Rautiainen <timo.rautiainen94@gmail.com>
2022-10-15 23:31:14 +0300

    fix code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,14 @@
 const removeFromArray = function(arr) {
-    arr.splice(2, 1);
+    for (let i = arr.length - 1; i >= 0; i--) {
+        for (let j = arguments.length - 1; j > 0; j--) {
+            if (arr[i] == arguments[j]) {
+                arr.splice(i, 1);
+            }
+        }
+    }
     return arr;
-};
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
