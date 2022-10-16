e08c9414eafae5fe171864d43aafe7e30639d6ed
Timo Rautiainen <timo.rautiainen94@gmail.com>
2022-10-16 22:41:21 +0300

    all tests pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
 const sumAll = function(start, end) {
+    if (start < 0 || end < 0 || typeof start != "number" || typeof end != "number") {
+        return "ERROR";
+    }
     let sum = 0;
+    if (start > end) {
+        for (let i = end; i <= start; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
     for (let i = start; i <= end; i++) {
         sum = sum + i;
     }
