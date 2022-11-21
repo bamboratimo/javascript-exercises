185ae5122409fff42af97dbcba38a25751cedc7d
Timo Rautiainen <timo.rautiainen94@gmail.com>
2022-11-22 01:00:41 +0200

    all tests pass

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  let wordArr = Array.from(word);
+  wordArr.forEach((elem) => {
+    if (elem.includes("!") || elem.includes(".")) {
+      wordArr.splice(-1, 1);
+    }
+  })
+  let orgStringArr = Array.from(word);
+  orgStringArr.forEach((elem) => {
+    if (elem.includes("!") || elem.includes(".")) {
+      orgStringArr.splice(-1, 1);
+    }
+  })
+  let orgString = orgStringArr.join("").toUpperCase();
+  orgString = orgString.split(" ").join("").split(",").join("");
+  let reversed = wordArr.reverse();
+  let reversedString = reversed.join("").toUpperCase();
+  reversedString = reversedString.split(" ").join("").split(",").join("");
+  if (reversedString === orgString) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
