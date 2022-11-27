dba54e1932bb87bbf5eca041f00dacf436cfad9c
Timo Rautiainen <timo.rautiainen94@gmail.com>
2022-11-27 21:38:13 +0200

    first test pass

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let arr = [1, 1];
+    for (let i = 0; i <= num; i++) {
+        arr.push((arr[arr.length-1] + arr[arr.length-2]));
+    }
+    return arr[num - 1];
 };
 
 // Do not edit below this line
