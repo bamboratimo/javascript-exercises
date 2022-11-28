8753538eb624306d0b5db543d09af1b262e427eb
Timo Rautiainen <timo.rautiainen94@gmail.com>
2022-11-28 23:11:35 +0200

    all tests pass

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const d = new Date();
+    let year = d.getFullYear();
+    arr.forEach(dude => {
+        if (!dude.yearOfDeath) {
+            dude.yearOfDeath = year;
+        }
+        dude.age = dude.yearOfDeath - dude.yearOfBirth;
+    });
+    arr.sort((a, b) => a.age - b.age);
+    return arr[arr.length - 1];
 };
 
 // Do not edit below this line
